bcbcdd93916c0b174acae76f71b6273a3173429e
yashtilala412 <yashtilala412@gmail.com>
2026-02-21 11:02:11 +0530

    feat:change site status to site url

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.status),
+    path('admin/', admin.site.urls),
     path('api/', include('apps.urls')), # This prefixes all your app URLs with 'api/'
 ]
